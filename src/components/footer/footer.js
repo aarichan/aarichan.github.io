21d02ed732a77a78441ab3aaea2a95ce0babf3f9
Aaric Han <72000441+aarichan@users.noreply.github.com>
2021-04-26 04:29:26 -0400

    Testing Google Search COnsole

---
+++ src/components/footer/footer.js
@@ -0,0 +1,7 @@
+/**
+ * footer.js
+ * By: Aaric Han
+ * 
+ * Created: 04/26/21
+ * Last Modified: 04/26/21
+ */
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,7 @@
+/**
+ * navbar.js
+ * By: Aaric Han
+ * 
+ * Created: 04/26/21
+ * Last Modified: 04/26/21
+ */
