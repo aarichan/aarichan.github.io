bc7e5b3ed87c7ee4763455b522f80be26d71d126
Aaric Han <72000441+aarichan@users.noreply.github.com>
2021-04-30 16:05:15 -0400

    Revert PLz

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -23,7 +23,7 @@
           <br/>
           <img src={portrait} className="App-logo" alt="logo" />
           <br/>
-          <h1 className="head-disp">Problem Solver<span id="the-dot"> &#183; </span>Solution Builder<span id="the-dot"> &#183; </span>Lifelong Learner</h1>
+          <h1>Problem Solver <span>&#183;</span> Solution Builder <span>&#183;</span> Lifelong Learner</h1>
         </header>
       </div>
      );
